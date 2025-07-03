5e332f431570434480a164f45a7e135140ac1649
Riken Sonani <rikenfree@gmail.com>
2025-07-03 11:07:09 +0530

    Ui3725

--- Assets/Scripts/Behaviours/CustomLoding2.cs
+++ Assets/Scripts/Behaviours/CustomLoding2.cs
@@ -27,7 +27,6 @@ public class CustomLoding2 : MonoBehaviour
 
         if (!loadingDone)
         {
-            Debug.Log("aa");
             if (welcomeProgress.fillAmount != 1f)
             {
                 welcomeProgress.fillAmount = welcomeProgress.fillAmount + Time.deltaTime * fillDuration;
