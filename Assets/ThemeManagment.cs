6bbb736156aeeea12c624298de4f5dae21a32a18
Riken Sonani <50581917+rikenfree@users.noreply.github.com>
2025-07-07 18:17:59 +0530

    ThemeManagerFunctionalityMake

---
+++ Assets/ThemeManagment.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ThemeManagment : MonoBehaviour
+{
+    public Type type;
+    Image image;
+
+    private void OnEnable()
+    {
+        image = GetComponent<Image>();
+
+        if(type == Type.topBar)
+        {
+            image.color = GetColorFromPrefs("topBar");
+        }
+    }
+
+   
+
+
+    public static Color GetColorFromPrefs(string playerPrefsKey)
+    {
+        if (PlayerPrefs.HasKey(playerPrefsKey))
+        {
+            string hex = PlayerPrefs.GetString(playerPrefsKey);
+            Color color;
+            if (ColorUtility.TryParseHtmlString(hex, out color))
+            {
+                return color;
+            }
+            else
+            {
+                return Color.white;
+                Debug.LogWarning("Invalid Hex Color in key: " + playerPrefsKey + " → Value: " + hex);
+            }
+        }
+        else
+        {
+            Debug.LogWarning("No color found in PlayerPrefs with key: " + playerPrefsKey);
+        }
+
+        return Color.white;
+    }
+
+
+    public enum Type
+    {
+        none,
+        button,
+        topBar,
+        headertext,
+        buttontext,
+        normaltext,
+        background
+
+    }
+}
